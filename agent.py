8b8e2ee0be9933e07d1c5701e88f3c3142f9ebfd
Jaden Padua <paduajaden@gmail.com>
2022-02-11 00:39:19 -0800

    RL Snake MVP

--- agent.py
+++ agent.py
@@ -3,42 +3,23 @@ import random
 import numpy as np
 from collections import deque
 from game import SnakeGameAI, Direction, Point
+from model import Linear_QNet, QTrainer
+from helper import plot
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
 LR = 0.001
 
-"""
-@description: AI Agent used to play our snake game with our model
-"""
-
 
 class Agent:
-    """
-    @description: init agent with certain properties
-        - epsilon = randomness
-        - gamma = discount rate
-    """
 
     def __init__(self):
-        self.games = 0
-        self.epsilon = 0
-        self.gamma = 0
-        self.memory = deque(maxlen=MAX_MEMORY)
-
-        self.model = None
-        self.trainer = None
-
-        # TODO, model, trainer
-    """
-    @description: Calculates current 11 state list at our head position
-        - generate 4 points adjacent to the head
-        - generate 4 0-1 values for current game direction
-        - For each danger dir, we calc based on u,d,l,r if we get in to that danger dir, if 1 of them is true that danger dir is true
-        - then 4 states used to hold what move direction we are
-        - then 4 states ised to hold where food is located
-        - returns np array with list of boolean values as required
-    """
+        self.n_games = 0
+        self.epsilon = 0  # randomness
+        self.gamma = 0.9  # discount rate
+        self.memory = deque(maxlen=MAX_MEMORY)  # popleft()
+        self.model = Linear_QNet(11, 256, 3)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
 
     def get_state(self, game):
         head = game.snake[0]
@@ -58,21 +39,25 @@ class Agent:
             (dir_l and game.is_collision(point_l)) or
             (dir_u and game.is_collision(point_u)) or
             (dir_d and game.is_collision(point_d)),
+
             # Danger right
             (dir_u and game.is_collision(point_r)) or
             (dir_d and game.is_collision(point_l)) or
             (dir_l and game.is_collision(point_u)) or
             (dir_r and game.is_collision(point_d)),
+
             # Danger left
             (dir_d and game.is_collision(point_r)) or
             (dir_u and game.is_collision(point_l)) or
             (dir_r and game.is_collision(point_u)) or
             (dir_l and game.is_collision(point_d)),
+
             # Move direction
             dir_l,
             dir_r,
             dir_u,
             dir_d,
+
             # Food location
             game.food.x < game.head.x,  # food left
             game.food.x > game.head.x,  # food right
@@ -81,105 +66,88 @@ class Agent:
         ]
 
         return np.array(state, dtype=int)
-    """
-    @description: Remembers tuple of variables and stores in our deque
-        - NOTE: when entries exceed 100,000, popleft() is called
-    """
 
     def remember(self, state, action, reward, next_state, done):
+        # popleft if MAX_MEMORY is reached
         self.memory.append((state, action, reward, next_state, done))
-
-    """
-    @description: 
-        - Takes random batch sample if memory is above 10,000
-        - If below 10,000 we let our sample be the entire memory
-        - Then zips the sample into multiple tuples with the respective vars inside
-        - Sends these tuples of data into long term training step
-    """
 
     def train_long_memory(self):
         if len(self.memory) > BATCH_SIZE:
-            sample = random.sample(self.memory, BATCH_SIZE)
+            mini_sample = random.sample(
+                self.memory, BATCH_SIZE)  # list of tuples
         else:
-            sample = self.memory
+            mini_sample = self.memory
 
-        states, actions, rewards, next_states, dones = zip(*sample)
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
         self.trainer.train_step(states, actions, rewards, next_states, dones)
-
-    """
-    @description: Takes state vars for (1 step) and sends it to our model to train
-    """
+        # for state, action, reward, nexrt_state, done in mini_sample:
+        #    self.trainer.train_step(state, action, reward, next_state, done)
 
     def train_short_memory(self, state, action, reward, next_state, done):
         self.trainer.train_step(state, action, reward, next_state, done)
-        pass
-
-    """
-    @description: Calculates what action we should take
-        - Tradeoff between exploration vs. explotation
-        - In the beggining we elect for randomness and explore
-        - As model gets more defined we elect to exploit that instead
-        - more games --> smaller epsilon --> more exploitation
-        - if we exploit we pass our state list into tensor
-        - obtain prediction --> get index with max argument (max probability)
-        - set final move of that index to 1
-    """
 
     def get_action(self, state):
-        self.epsilon = 80 - self.games
+        # random moves: tradeoff exploration / exploitation
+        self.epsilon = 80 - self.n_games
         final_move = [0, 0, 0]
-        # Exploration
-        if random.randint(0, 200) < self.epsilon:
+        if random.randint(0, 200) < self.epsilon and False:
             move = random.randint(0, 2)
             final_move[move] = 1
-        # Exploitation
         else:
             state0 = torch.tensor(state, dtype=torch.float)
-            prediction = self.model.predict(state0)
+            prediction = self.model(state0)
             move = torch.argmax(prediction).item()
             final_move[move] = 1
 
         return final_move
 
 
-"""
-@description: Init method which kicks off the training and instantiates game + agent
-    - defines variables to plot later
-    - kicks off training loop to train model until we exit out
-    - get old state --> predict final move --> apply final move --> get new state
-    - saves our Model if we get a high score
-"""
-
-
 def train():
     plot_scores = []
     plot_mean_scores = []
     total_score = 0
-    record = 0
+    record = 50
     agent = Agent()
     game = SnakeGameAI()
-    # Training loop
+
+    agent.model.load_state_dict(torch.load("./model/model.pth"))
+    agent.model.eval()
+
     while True:
+        # get old state
         state_old = agent.get_state(game)
+
+        # get move
         final_move = agent.get_action(state_old)
+
+        # perform move and get new state
         reward, done, score = game.play_step(final_move)
         state_new = agent.get_state(game)
 
+        # train short memory
         agent.train_short_memory(
-            state_old, final_move. reward, state_new, done)
+            state_old, final_move, reward, state_new, done)
 
-        agent.remember(state_old, final_move. reward, state_new, done)
+        # remember
+        agent.remember(state_old, final_move, reward, state_new, done)
 
         if done:
+            # train long memory, plot result
             game.reset()
-            games += 1
+            agent.n_games += 1
             agent.train_long_memory()
 
             if score > record:
                 record = score
                 agent.model.save()
 
-            print('Game:', agent.games, 'Score:', score, 'Record:', record)
+            print('Game', agent.n_games, 'Score', score, 'Record:', record)
+
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
 
 
 if __name__ == '__main__':
